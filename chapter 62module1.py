0261c5f42bc51fb04d6301f123c7855ee4d9fd8c
Jordan Stevens <jsteve40@its.jnj.com>
2024-06-19 10:45:57 +0100

    up to chapter 66

---
+++ chapter 62module1.py
@@ -0,0 +1,11 @@
+#chapter 62 module 1
+
+# name is going to be assigned a string of __main__
+
+#print(__name__)
+
+# import module2 and we can see if we print name it appears as __main__ and print chapter62module2.__name__ appears as chapter62module2
+import chapter62module2
+print(__name__)
+print(chapter62module2.__name__)
+
---
+++ chapter62module2.py
@@ -0,0 +1,7 @@
+# chapter 62 module 2
+
+# print(__name__)
+
+import chapter62module1
+print(__name__)
+print(chapter62module1.__name__) #print name varibale of module 1 which is being imported
